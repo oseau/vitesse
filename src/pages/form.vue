8910ba2d1372dbcb565d51fd860ddfa7825b3ac0
yang <oseau@me.com>
2021-07-09 05:26:24 +0800

    minimal reproduce

---
+++ src/pages/form.vue
@@ -0,0 +1,18 @@
+<template>
+  <textarea
+    id="about"
+    name="about"
+    rows="3"
+    class="
+      shadow-sm
+      focus:ring-indigo-500 focus:border-indigo-500
+      mt-1
+      block
+      w-full
+      sm:text-sm
+      border border-gray-300
+      rounded-md
+    "
+    placeholder="you@example.com"
+  ></textarea>
+</template>
